3db9bd3d2f619f2e873695e5e38b1fb5d1f701b9
Sujal-code123 <sujalbondarde18@gmail.com>
2026-03-16 19:54:48 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -23,8 +23,7 @@ OPENAI_API_BASE = os.getenv("OPENAI_API_BASE", "https://openrouter.ai/api/v1")
 #MODEL_NAME = os.getenv("MODEL_NAME", "deepseek/deepseek-r1-0528:free")
 #MODEL_NAME = os.getenv("MODEL_NAME", "openai/gpt-oss-120b:free")
 #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_API_BASE = os.getenv("OPENAI_API_BASE", "https://api.openai.com/v1")
-MODEL_NAME = os.getenv("MODEL_NAME", "gpt-4o")
+
 
 from langchain_openai import ChatOpenAI
 
